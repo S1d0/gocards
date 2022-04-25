3046829e9280b94cf38259babb4532b0fdfb788d
sebastian <idzkowski.s@gmail.com>
2022-04-25 16:17:09 +0200

    Added 1st test

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -29,4 +36,35 @@ func (d deck) print() {
 
 func deal(d deck, handsize int) (deck, deck) {
 	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), "")
+}
+
+func (d deck) shufleDeck() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
+}
+
+func (d deck) saveToFile(filename string) error {
+	fmt.Printf("Saving deck %v to file %v\n", d, filename)
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	fmt.Printf("Reading deck from file: %v\n", filename)
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		// Log the error and entirly quit the program
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	slice := strings.Split(string(bs), ",")
+	return deck(slice)
 }
--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 func main() {
 	cards := newDeck()
+	cards.shufleDeck()
+
 	cards.print()
+	// cards.print()
 
-	hand, remainingDeck := deal(cards, 5)
+	// hand, remainingDeck := deal(cards, 5)
 
-	hand.print()
-	remainingDeck.print()
+	// hand.print()
+	// remainingDeck.print()
+	// fmt.Println(hand.toString())
+	// hand.saveToFile("myfile.txt")
 
+	// newCarsd := newDeckFromFile("myfile1.txt")
+	// fmt.Println("Cards from file: ", newCarsd)
 }
